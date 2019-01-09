2adbc9901b0119785f3b97cb30bb05e0e7142a76
anil-plivo <anil.kumar@plivo.com>
2019-01-09 13:10:23 +0530

    latest develop binaries

--- PlivoAddressBook/my_bs.sh
+++
@@ -1,4 +0,0 @@
-rm -rf app/libs/*.aar
-#scp -i /Users/anil/Downloads/forandroid.pem ubuntu@13.233.184.227:/home/ubuntu/2.8_all/plivo-android-sdk/build/outputs/aar/*.aar app/libs/
-cp -r /Users/anil/Desktop/office/cflag/flx/plv/plivo-android-sdk/build/outputs/aar/* app/libs/
-./gradlew clean assembleDebug
