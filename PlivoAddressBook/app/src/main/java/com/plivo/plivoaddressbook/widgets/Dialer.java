9753037d0c47119b5626f247901b1681e17153b7
anil-plivo <anil.kumar@plivo.com>
2019-01-30 15:50:22 +0530

    CSDK-351, CSDK-357 disable dtmf and soft keyboard

--- PlivoAddressBook/app/src/main/java/com/plivo/plivoaddressbook/widgets/Dialer.java
+++ PlivoAddressBook/app/src/main/java/com/plivo/plivoaddressbook/widgets/Dialer.java
@@ -14,6 +14,7 @@ import android.widget.GridView;
 import android.widget.ListAdapter;
 
 import com.plivo.plivoaddressbook.R;
+import com.plivo.plivoaddressbook.model.Call;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatButton;
