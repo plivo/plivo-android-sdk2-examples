883f77848408367fc4af635b4ee7d2b6ace5f110
Anand S <anand@plivo.com>
2019-05-13 13:47:25 +0530

    added functionality to send certid during login

--- PlivoAddressBook/app/src/main/java/com/plivo/plivoaddressbook/layer/impl/PlivoSDKImpl.java
+++ PlivoAddressBook/app/src/main/java/com/plivo/plivoaddressbook/layer/impl/PlivoSDKImpl.java
@@ -46,7 +46,7 @@ public class PlivoSDKImpl extends PlivoBackend implements EventListener {
 
     public boolean login(User user, PlivoBackendListener.LoginListener listener) {
         super.login(user, listener);
-        return endpoint().login(user.getUsername(), user.getPassword());
+        return endpoint().login(user.getUsername(), user.getPassword(), user.getDeviceToken(), "testcertId");
     }
 
     public boolean logout(PlivoBackendListener.LogoutListener listener) {
