f12d1dca625590d8b4bcb13ec86065d877b73a42
rdhek-plivo <rahul.dhek@plivo.com>
2019-08-29 12:21:17 +0530

    added mute unmute option

--- PlivoSimpleQuickStart/app/src/main/java/com/plivo/plivosimplequickstart/MainActivity.java
+++ PlivoSimpleQuickStart/app/src/main/java/com/plivo/plivosimplequickstart/MainActivity.java
@@ -50,6 +50,9 @@ public class MainActivity extends AppCompatActivity implements PlivoBackEnd.Back
 
     private int tick;
 
+    Boolean isHold = false;
+    Boolean isMute = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -132,6 +135,8 @@ public class MainActivity extends AppCompatActivity implements PlivoBackEnd.Back
 
         String title = state.name() + " " + (outgoing != null ? Utils.to(outgoing.getToContact()) : "");
         CharSequence btnText = getString(R.string.call);
+        CharSequence holdText = getString(R.string.hold);
+        CharSequence muteText = getString(R.string.mute);
         boolean cancelable = true;
         boolean showAlert = false;
         switch (state) {
@@ -161,6 +166,28 @@ public class MainActivity extends AppCompatActivity implements PlivoBackEnd.Back
                             outgoing.hangup();
                         }
                     })
+                    .setNegativeButton(holdText,((dialog, which) -> {
+                        if(isHold){
+                            outgoing.unhold();
+                        }else{
+                            //if(state == STATE.ANSWERED && !isHold) {
+                                outgoing.hold();
+                            //}
+                        }
+                        updateHoldFlag();
+                        showOutCallUI(state,outgoing);
+                    }))
+                    .setPositiveButton(muteText,((dialog, which) -> {
+                        if(isMute){
+                            outgoing.unmute();
+                        }else{
+                            //if(state == STATE.ANSWERED && !isMute) {
+                                outgoing.mute();
+                            //}
+                        }
+                        updateHoldFlag();
+                        showOutCallUI(state,outgoing);
+                    }))
                     .show();
 
             if (state == STATE.ANSWERED) startTimer();
@@ -194,6 +221,15 @@ public class MainActivity extends AppCompatActivity implements PlivoBackEnd.Back
             });
         }
 
+    }
+
+    void updateHoldFlag(){
+        isHold = !isHold;
+    }
+
+
+    void updateMuteFlag(){
+        isMute = !isMute;
     }
 
     /**
